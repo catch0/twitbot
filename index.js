29f15d67993489c27cd2c911fc5cc4e9071de035
Spence Peacock <spencerpeacock@gmail.com>
2017-10-31 11:41:30 -0500

    adjusted twitter account to henry_loudsound

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const T = new twit(config);
 
 function searchTwitter() {
   const params = {
-    q: '@willfisher4cong',
-    count: 5,
+    q: '#gostars',
+    count: 100,
     result_type: ['recent', 'popular'],
     language: 'en'
   }
   return T.get('search/tweets', params)
   .then(function(res){
-    const tweet = res.data.statuses[0];
+    //grabbing the first
+        // const tweet = res.data.statuses[0];
+    //grabbing a random one
+    const tweet = res.data.statuses[Math.floor(Math.random() * res.data.statuses.length)];
     if(!tweet){
       throw new Error('No Status found');
     }
@@ -20,14 +23,43 @@ function searchTwitter() {
   });
 }
 
+function postTweet(url, params){
+    return T.post(url, params, function(err){
+      if(err){
+        console.log('ohh shit something dun fucked up with the posting', err);
+        return;
+      }
+      console.log('Post Tweet Successful!')
+    });
+}
+
+//right now this just retweets any mention
 function retweet(){
   return searchTwitter()
   .then(function(status){
-    return T.post('statuses/retweet/:id', {id: status.id_str});
+    return postTweet('statuses/retweet/:id', {id: status.id_str})
+    console.log(status)
   })
   .catch(function(err){
     console.log("error retweeting", err)
   });
 }
 
+function favourite(){
+  return searchTwitter()
+  .then(function(status){
+    return T.post('favorites/create', {id: status.id_str}, function(err){
+      if(err){
+        console.log('ohh shit something dun fucked up');
+        return;
+      }
+      console.log('Favorite Successful!')
+    });
+  })
+  .catch(function(err){
+    console.log("error favoriting", err)
+  });
+}
+
 retweet();
+// favourite();
