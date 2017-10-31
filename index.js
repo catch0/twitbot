4e196e34d7c513092761928032f53739e5bb27f5
Spence Peacock <spencerpeacock@gmail.com>
2017-10-31 00:07:12 -0500

    retweet functionality working

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ const T = new twit(config);
 
 function searchTwitter() {
   const params = {
-    q: '@WillFisher4Cong',
+    q: '@willfisher4cong',
     count: 5,
     result_type: ['recent', 'popular'],
     language: 'en'
   }
-  T.get('search/tweets', params)
+  return T.get('search/tweets', params)
   .then(function(res){
     const tweet = res.data.statuses[0];
     if(!tweet){
       throw new Error('No Status found');
     }
-
-    const params = {id: tweet.id_str};
-    console.log(params);
+    return tweet;
   });
 }
 
-searchTwitter();
+function retweet(){
+  return searchTwitter()
+  .then(function(status){
+    return T.post('statuses/retweet/:id', {id: status.id_str});
+  })
+  .catch(function(err){
+    console.log("error retweeting", err)
+  });
+}
+
+retweet();
