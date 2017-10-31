758b049c4fe4655e4bc20589e5c387399b4ac11c
Spence Peacock <spencerpeacock@gmail.com>
2017-10-30 22:52:02 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+  module.exports = {
+    CONSUMER_KEY: process.env.CONSUMER_KEY,
+    CONSUMER_SECRET: process.env.CONSUMER_SECRET,
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET
+}
