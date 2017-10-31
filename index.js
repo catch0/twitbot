63a358d992f58d4549bd58ddd1eebb10bf65253c
Spence Peacock <spencerpeacock@gmail.com>
2017-10-31 14:31:39 -0500

    can retweet, favorite and create a new tweet

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const config = require('./config');
 const twit = require('twit');
-
 const T = new twit(config);
 
+
+//this function searches twitter for or parameters
 function searchTwitter() {
   const params = {
     q: '#gostars',
@@ -23,37 +24,36 @@ function searchTwitter() {
   });
 }
 
+//this post actually posts the tweet using built in functions from Twit
 function postTweet(url, params){
     return T.post(url, params, function(err){
       if(err){
-        console.log('ohh shit something dun fucked up with the posting', err);
+        console.log('ohh shit something dun fucked up ', err);
         return;
       }
-      console.log('Post Tweet Successful!')
+      console.log('fuck yeah it worked !')
     });
 }
 
-//right now this just retweets any mention
-function retweet(){
-  return searchTwitter()
+//this function grabs a tweet we are looking for
+function searchAndPostTweet(url){
+  return function(){
+    return searchTwitter()
   .then(function(status){
-    return postTweet('statuses/retweet/:id', {id: status.id_str})
-    console.log(status)
+    return postTweet(url, {id: status.id_str})
   })
   .catch(function(err){
-    console.log("error retweeting", err)
+    console.log("htere is an error inside the search and PostTweet", err)
   });
 }
-
-function favourite(){
-  return searchTwitter()
-  .then(function(status){
-    return postTweet('favorites/create', {id: status.id_str})
-  })
-  .catch(function(err){
-    console.log("error favoriting", err)
-  });
 }
 
+const retweet = searchAndPostTweet('statuses/retweet/:id');
+const favorite = searchAndPostTweet('favorites/create');
+const newTweet = function(){
+  return postTweet('statuses/update', {status: 'This is a test from my Awesome Twitter Bot that I bulit with node.js !!!'})
+}
+
+newTweet();
 // retweet();
-favourite();
+// favorite();
