944b72ebecc508a56776a524f885e0e656f5be3f
Spence Peacock <spencerpeacock@gmail.com>
2017-10-30 23:18:07 -0500

    Twit get function

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
   module.exports = {
-    CONSUMER_KEY: process.env.CONSUMER_KEY,
-    CONSUMER_SECRET: process.env.CONSUMER_SECRET,
-    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const config = require('./config');
+const twit = require('twit');
+
+const T = new twit(config);
+
+function searchTwitter() {
+  const params = {
+    q: 'trek bikes',
+    count: 5,
+    result_type: ['recent', 'popular'],
+    language: 'en'
+  }
+  T.get('search/tweets', params)
+  .then(function(res){
+    console.log(res.data.statuses);
+  });
+}
+
+searchTwitter();
