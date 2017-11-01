9789e85a552cbf73a9a74707d3a886d9c1f5fb81
Spence Peacock <spencerpeacock@gmail.com>
2017-11-01 13:35:04 -0500

    added more params for findtweets included exact phrase matching, and started replybot

--- findtweets.js
+++ findtweets.js
@@ -4,17 +4,32 @@ const twit = require('twit');
 const T = new twit(config);
 const stream = T.stream('user');
 
-
+//grabs a tween when the name michael burgess is mentioned
 const params = {
-  q: 'AHS',
-  count: 5
+  q: "\"Michael Burgess\"",
+  count: 20,
+  language: 'en'
+};
+// grabs a tweet when his handle is actually mentioned
+const params2 = {
+  q: "@michaelcburgess",
+  count: 10,
+  language: 'en'
+};
+// grabs tweets when someone says linsey is running against burgess
+const params3 = {
+  q: "@linseyfagantx is running against",
+  count: 5,
+  language: 'en'
 };
 
-T.get('search/tweets', params, gotData);
+T.get('search/tweets', params3, gotData);
+
 
  function gotData(err, data, response) {
    var tweets = data.statuses;
    for(var i = 0; i < tweets.length; i++){
      console.log(tweets[i].text);
+     console.log("/br")
    }
 };
