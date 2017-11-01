773373e5d977459d42d4eeb386f61cd2f6d131e8
Spence Peacock <spencerpeacock@gmail.com>
2017-11-01 17:36:47 -0500

    adjusted find tweets

--- findtweets.js
+++ findtweets.js
@@ -8,17 +8,18 @@ const stream = T.stream('user');
 const params = {
   q: "\"Michael Burgess\"",
   count: 20,
-  language: 'en'
+  language: 'en',
+  result_type: 'recent'
 };
 // grabs a tweet when his handle is actually mentioned
 const params2 = {
-  q: "@michaelcburgess",
+  q: "fagan is running",
   count: 10,
   language: 'en'
 };
 // grabs tweets when someone says linsey is running against burgess
 const params3 = {
-  q: "@linseyfagantx is running against",
+  q: "@linseyfagantx is running ",
   count: 5,
   language: 'en'
 };
@@ -29,7 +30,9 @@ T.get('search/tweets', params3, gotData);
  function gotData(err, data, response) {
    var tweets = data.statuses;
    for(var i = 0; i < tweets.length; i++){
-     console.log(tweets[i].text);
-     console.log("/br")
+     console.log("tweet" + (i+1));
+     console.log("Text:  " + tweets[i].text);
+     console.log("User:   " + tweets[i].user.name);
+     console.log("Time:" + tweets[i].created_at);
    }
 };
