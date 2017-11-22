0a887ec695debb0b5dd7f52a204e76306d32a8ae
Spence Peacock <spencerpeacock@gmail.com>
2017-11-21 20:19:56 -0600

    grab print function working

--- grabprint.js
+++ grabprint.js
@@ -6,14 +6,12 @@ var T = new Twit(config);
 
 T.get('statuses/user_timeline', {screen_name: 'brandnew535', language: 'en'} , gotData);
  function gotData(err, data, response) {
-   var tweets = data.statuses;
    console.log(data)
-  //  for(var i = 0; i < tweets.length; i++){
-  //    console.log("tweet" + (i+1));
-  //    console.log("Text:  " + tweets[i].text);
-  //    console.log("User:   " + tweets[i].user.name);
-  //    console.log("Time:" + tweets[i].created_at);
-  //    console.log("status id" + tweets[i].id);
-  //    console.log(tweets[i])
-  // }
+   for(var i = 0; i < data.length; i++){
+     console.log("tweet" + (i+1));
+     console.log("Text:  " + data[i].text);
+     console.log("User:   " + data[i].user.name);
+     console.log("Time:" + data[i].created_at);
+     console.log("status id" + data[i].id);
+  }
 }
